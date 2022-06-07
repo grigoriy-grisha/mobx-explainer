e939140f8b98a45b58cd4af939363bec1766703a
Gavrilov Grigoriy <g.gavrilov@worksolutions.ru>
2022-06-07 17:35:37 +0300

    feat. Глобавльное состояние проверка работы HOC observer

---
+++ src/globalExmpleState.js
@@ -0,0 +1,9 @@
+import {observableArray} from "./package/simple-mobx";
+
+class GlobalExmpleState {
+    constructor() {
+        this.array = observableArray([1,2,3,4])
+    }
+}
+
+export const globalExmpleState = new GlobalExmpleState()
