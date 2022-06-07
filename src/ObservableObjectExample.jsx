0108e7bf9aedad40e7ec8c2e7d79f3c44b2ed484
Gavrilov Grigoriy <g.gavrilov@worksolutions.ru>
2022-06-07 13:49:24 +0300

    feat. Реализован функционал для демонстрации отписки реакций от наблюдаемых значений

--- src/ObservableObjectExample.jsx
+++ src/ObservableObjectExample.jsx
@@ -1,19 +1,20 @@
-import {useEffect, useMemo, useState} from 'react'
-import {autorun} from "./package/simple-mobx";
-import {observableObject} from "./package/simple-mobx";
+import {useEffect, useMemo, useRef, useState} from 'react'
+import {autorun, observableObject} from "./package/simple-mobx";
 
 function ObservableObjectExample() {
-    const observableHelloWold = useMemo(() => observableObject({hello:'hello'}), [])
-    const [value, setValue] = useState(() => observableHelloWold.hello)
+  const observableHelloWold = useMemo(() => observableObject({hello: 'hello'}), [])
+  const [value, setValue] = useState(() => observableHelloWold.hello)
+  const disposerRef = useRef()
 
-    useEffect(() => {
-        autorun(() => setValue(observableHelloWold.hello))
-    }, [])
+  useEffect(() => {
+    disposerRef.current = autorun(() => setValue(observableHelloWold.hello))
+  }, [])
 
-    return <div>
-        <input type="text" value={value} onChange={event => observableHelloWold.hello = event.target.value}/>
-        <div>{value}</div>
-    </div>
+  return <div>
+    <input type="text" value={value} onChange={event => observableHelloWold.hello = event.target.value}/>
+    <div>{value}</div>
+    <div onClick={disposerRef.current}>dispose</div>
+  </div>
 }
 
 export default ObservableObjectExample
