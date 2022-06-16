fc23219fbb12e719425e8edbfbefd0821b57a316
Gavrilov Grigoriy <g.gavrilov@worksolutions.ru>
2022-06-16 14:49:17 +0300

    test. тесты на связки observableArray с autorun

---
+++ src/package/simple-mobx/__test__/observableArray-auorun.test.js
@@ -0,0 +1,46 @@
+import { observableObject } from "../ObservableObject";
+import { observableArray } from "../ObservableArray";
+
+import { autorun } from "../autorun";
+
+import { expectListenerWasUpdated } from "./utils";
+
+describe("observableArray", () => {
+  it("observableArray must be defined", function () {
+    expect(observableObject).toBeDefined();
+  });
+
+  it("autorun must be defined", function () {
+    expect(autorun).toBeDefined();
+  });
+
+  it("listener must be called", function () {
+    const observable = observableArray([1, 2, 3, 4, 5]);
+    const listener = jest.fn(() => observable[0]);
+
+    autorun(listener);
+
+    observable[0] = "new hello";
+
+    expectListenerWasUpdated(listener);
+  });
+
+  it("listener must not be called before dispose autorun", function () {
+    const observable = observableArray([1, 2, 3, 4, 5]);
+    const listener = jest.fn(() => observable[0]);
+
+    const dispose = autorun(listener);
+
+    observable[0] = "new hello";
+
+    const expectListenerIsNoWastUpdated = expectListenerWasUpdated(listener);
+
+    dispose();
+
+    observable[0] = "new hello 1";
+    observable[0] = "new hello 2";
+    observable[0] = "new hello 3";
+
+    expectListenerIsNoWastUpdated();
+  });
+});
